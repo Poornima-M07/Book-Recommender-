ad6d03bc4ec9e297a4f0888b9089ff49390b5391
Poornima-M07 <poornimamurali.270607@gmail.com>
2025-11-28 18:58:06 +0530

    Add files via upload

---
+++ Book_recommendation.py
@@ -0,0 +1,33 @@
+import requests
+import random
+age = int(input("Enter age: "))
+genre = input("Enter genre: ").lower()
+
+if age < 12:
+    age_group = "children"
+else:
+    age_group = ""  
+
+url = f"https://openlibrary.org/subjects/{genre}.json?limit=20"
+response = requests.get(url).json()
+books = response.get("works", [])
+random.shuffle(books) 
+
+count = 0
+for book in response.get("works", []):
+    subjects = [s.lower() for s in book.get("subject", [])]
+    
+   
+    if age_group == "children" and "children" not in subjects:
+        continue
+    if genre not in subjects:
+        continue
+    
+    print("\nTitle:", book.get("title", "No title"))
+    authors = book.get("authors", [])
+    author_names = [a["name"] for a in authors]
+    print("Author(s):", ", ".join(author_names) if author_names else "Unknown")
+    
+    count += 1
+    if count == 10:  
+        break
